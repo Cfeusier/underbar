aee68680f5bb2c4944aba8a3a4c475940152a7d3
Cfeusier <cfeusier@gmail.com>
2014-11-19 20:12:13 -0800

    Finish #reject implementation using #filter

--- src/underbar.js
+++ src/underbar.js
@@ -93,6 +93,11 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var opposite = _.filter(collection, test);
+
+    return _.filter(collection, function(item) {
+      return opposite.indexOf(item) < 0;
+    });
   };
 
   // Produce a duplicate-free version of the array.
