b743e6c411cb3a9ad2b847bbafd978031556aa07
Cfeusier <cfeusier@gmail.com>
2014-11-23 11:33:48 -0800

    Finish #some implementation

--- src/underbar.js
+++ src/underbar.js
@@ -203,6 +203,11 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    var none = _.every(collection, function(item) {
+      return !iterator ? !item : !iterator(item);
+    });
+
+    return !none || false;
   };
 
 
