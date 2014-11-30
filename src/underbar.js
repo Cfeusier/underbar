af720517d622be1c4e93ca6f2fe2a9983ba9965d
Cfeusier <cfeusier@gmail.com>
2014-11-30 12:47:34 -0800

    Finish #flatten implementation in functional style (extra)

--- src/underbar.js
+++ src/underbar.js
@@ -370,6 +370,13 @@ var _ = {};
   //
   // Hint: Use Array.isArray to check if something is an array
   _.flatten = function(nestedArray, result) {
+    result = result || [];
+
+    _.each(nestedArray, function(item) {
+      Array.isArray(item) ? _.flatten(item, result) : result.push(item);
+    });
+
+    return result;
   };
 
   // Takes an arbitrary number of arrays and produces an array that contains
